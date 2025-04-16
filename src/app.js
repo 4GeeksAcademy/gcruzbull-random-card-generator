89d951f14c97a5841556cfdc7cd9af83933b10c3
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-04-16 01:10:23 +0000

    generador de cartas random listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let iconsArray = ["♦", "♥", "♠", "♣"]
+
+  let numberArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+  const generateRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)]
+
+  let randomIcon = generateRandomElement(iconsArray)
+  let randomNumber = generateRandomElement(numberArray)
+
+  console.log(randomIcon, randomNumber, randomIcon)
+
+  const iconTopElement = document.querySelector("#icon-top")
+  const numberElement = document.querySelector("#number")
+  const iconBottomElement = document.querySelector("#icon-bottom")
+
+  iconTopElement.innerHTML = randomIcon
+  numberElement.innerHTML = randomNumber
+  iconBottomElement.innerHTML = randomIcon
+  
+  if (randomIcon == "♦" || randomIcon == "♥") {
+    iconTopElement.style.color = "red"
+    iconBottomElement.style.color = "red"
+  } else {
+    iconTopElement.style.color = "black"
+    iconBottomElement.style.color = "black"
+  }
+  // console.log("Hello Rigo from the console!");
 };
